25959f3bc96a91b3d4c14dcbc1d7f2ff8f5a6041
Oberon Dixon-Luinenburg <dixonluinenburg@n0053.savio3>
2024-05-21 18:55:38 -0700

    Auto torch device in inference.py. Looking at results for hp-filtered megalodon CpG data. Made script to process chrX all 128bp chunks for comparison with motifs.

--- methylseqnet/.ipynb_checkpoints/inference-checkpoint.py
+++ methylseqnet/.ipynb_checkpoints/inference-checkpoint.py
@@ -3,8 +3,11 @@ from methylseqnet.methylseqnn import MethylSeqNN
 import json
 from pathlib import Path
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def load_for_eval(model_path: str | Path):
-    saved_model = torch.load(str(model_path)+'_state.pth')
+    
+    saved_model = torch.load(str(model_path)+'_state.pth',map_location=torch.device(device))
     with open(str(model_path)+'_meta.json', 'r') as f:
         metadata_list = json.load(f)
     hyperparams = metadata_list[0]
