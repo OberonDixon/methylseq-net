ad318142d1aa03cb2611d37a32573d246a83eedf
Oberon Dixon-Luinenburg <dixonluinenburg@n0126.savio4>
2024-07-23 13:47:59 -0700

    Added ability to run inference for just a specific output track, and only run samples that have unmasked loss for that track

--- methylseqnet/inference.py
+++ methylseqnet/inference.py
@@ -6,15 +6,17 @@ from pathlib import Path
 from methylseqnet.dataset import CustomH5Dataset
 from tqdm.auto import tqdm
 from torch.utils.data import DataLoader
-
+import gin
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def load_for_eval(model_path: str | Path):
     
     saved_model = torch.load(str(model_path)+'_state.pth')
-    with open(str(model_path)+'_meta.json', 'r') as f:
-        metadata_list = json.load(f)
+    operative_config_str = saved_model['gin_file']
+    gin.parse_config(operative_config_str)
+    # with open(str(model_path)+'_meta.json', 'r') as f:
+    #     metadata_list = json.load(f)
     # hyperparams = metadata_list[0]
     model = MethylSeqNN()
     model.load_state_dict(saved_model['model_state_dict'])
@@ -25,7 +27,15 @@ def run_whole_dataset(
     model_path: str | Path,
     dataset_path: str | Path,
     batch_size: int=64,
+    track_index: int|None = None,
 ):
+    """
+    This function takes an h5 dataset (could be train, valid, test, etc) and runs through inference end-to-end for all
+    samples, with a specified model (which must include it's own hyperparamter gin str). Batch size goes to a reasonable
+    default. If a track_index is specified AND the dataset has a mask, only the samples where the track index in question
+    is at least partly unmasked will be run through the model. Otherwise, all samples will be run.
+    """
+    
     model = load_for_eval(model_path).to(device)
     
     dataset = CustomH5Dataset(dataset_path,batch_size=batch_size)
@@ -35,16 +45,36 @@ def run_whole_dataset(
     outputs_list = []
     
     for inputs, targets, mask in tqdm(dataloader,unit='batch',desc='model passes'):
-        inputs, targets = inputs.to(device), targets.to(device)
 
         targets = targets.permute(0, 2, 1)
 
-        outputs = model(inputs)
+        # if mask is not None and track_index is not None, we want to subset the batch to 
+        # samples where the track_index is unmasked before we get to actually running a 
+        # forward pass of the model
         if mask is not None:
             mask = mask.permute(0, 2, 1)
+            if track_index is not None:
+                subset_mask = mask[:,track_index,:]
+                sample_indices = subset_mask.any(dim=-1)
+                inputs = inputs[sample_indices,:,:]
+                targets = targets[sample_indices,:,:]
+                mask = mask[sample_indices,:,:]
             mask.to(device)
-            targets = targets[mask]
-            outputs = outputs[mask]
+        else:
+            # for code clarity, we make a "fake" mask that is just True everywhere
+            # this means we don't need any other if statements to handle None, and
+            # it means the later mask application will still squeeze the targets and 
+            # outputs even if it doesn't remove any elements
+            mask = torch.ones_like(targets, dtype=torch.bool)
+            mask.to(device)
+
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        outputs = model(inputs)
+            
+        # this applies the appropriate masking and reshapes to 1d so we can directly extend the list
+        targets = targets[mask]
+        outputs = outputs[mask]
         targets_list.extend(targets.cpu().detach().numpy().tolist())
         outputs_list.extend(outputs.cpu().detach().numpy().tolist())
     
