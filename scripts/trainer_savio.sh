fed884ca21ed53b3b951f8905bb2decf22fdf298
Oberon Dixon-Luinenburg <dixonluinenburg@n0065.savio3>
2024-09-23 12:21:12 -0700

    MSN-54 Changed job ID tracking to hopefully work correctly on restart

--- scripts/trainer_savio.sh
+++ scripts/trainer_savio.sh
@@ -28,7 +28,7 @@ CONFIG_FILES=(
 # Get the config file for this array task
 CONFIG_FILE=${CONFIG_FILES[$SLURM_ARRAY_TASK_ID]}
 
-UNIQUE_IDENTIFIER="slurm${SLURM_JOB_ID}task${SLURM_ARRAY_TASK_ID}"
+UNIQUE_IDENTIFIER="slurm${SLURM_ARRAY_JOB_ID}task${SLURM_ARRAY_TASK_ID}"
 
 source activate methylseqnet
 NCCL_P2P_DISABLE=1 python ../methylseqnet/trainer.py --config $CONFIG_FILE --unique_identifier $UNIQUE_IDENTIFIER
