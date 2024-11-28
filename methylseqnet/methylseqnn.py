1416a3cfccd1b7daf6d550121dcbafb626fed17f
Oberon Dixon-Luinenburg <dixonluinenburg@n0118.savio3>
2024-11-28 10:30:31 -0800

    Adjusted folder structure again due to path issues. Removed methylseqnn shapes output

--- methylseqnet/methylseqnn.py
+++ methylseqnet/methylseqnn.py
@@ -87,10 +87,10 @@ class MethylSeqNN(L.LightningModule):
             outputs = outputs[mask]
             targets = targets[mask]
         loss = self.criterion(outputs, targets)
-        print(outputs.shape,targets.shape,mask.shape)
-        print(loss)
-        print(self.layers[0].channels)
-        print(self.layers[1].conv.weight.grad)
+        # print(outputs.shape,targets.shape,mask.shape)
+        # print(loss)
+        # print(self.layers[0].channels)
+        # print(self.layers[1].conv.weight.grad)
         self.log("train_loss", loss)
         return loss  
 
