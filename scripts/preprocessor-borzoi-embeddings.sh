d75ecce5f731b60421388b1bb29975f29684f2a3
Oberon Dixon-Luinenburg <dixonluinenburg@n0014.savio3>
2025-02-27 15:28:53 -0800

    Embedding saver script adjustments - 8hrs is too short.

--- scripts/preprocessor-borzoi-embeddings.sh
+++ scripts/preprocessor-borzoi-embeddings.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
-#SBATCH --time=8:00:00
+#SBATCH --time=15:00:00
 #SBATCH --output=/clusterfs/nilah/oberon/lightning/sbatch_logs/preprocess_methylseqnet_%A_%a.out
 #SBATCH --error=/clusterfs/nilah/oberon/lightning/sbatch_logs/preprocess_methylseqnet_%A_%a.err
 #SBATCH --array=0-7 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
@@ -27,7 +27,7 @@ FOLDS=(
 FOLD=${FOLDS[$SLURM_ARRAY_TASK_ID]}
 CONFIG_FILE="../configs/residual/borzoi_pretrain_head_only.gin"
 INPUT_DIR="/global/scratch/users/dixonluinenburg/atlas_datasets/borzoi-128-multimethyl-bisulfite-atac-cage/"
-OUTPUT_DIR="/global/scratch/users/dixonluinenburg/atlas_datasets/borzoi-embeddings/"
+OUTPUT_DIR="/global/scratch/users/dixonluinenburg/atlas_datasets/borzoi-embeddings-rerun/"
 
 source activate basenji2-pytorch
 python ../methylseqnet/preprocessor.py pretrained_model_embeddings --config $CONFIG_FILE --embeddings-shape 1920 6144 --input-datasets-directory $INPUT_DIR --output-datasets-directory $OUTPUT_DIR --subset $FOLD --batch-size 2 --write-batch-size 16
