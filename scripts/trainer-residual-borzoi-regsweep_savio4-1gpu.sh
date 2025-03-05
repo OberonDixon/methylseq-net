c421da8fea251ce6b07d7d6ebf6ddf2bedbff96a
Oberon Dixon-Luinenburg <dixonluinenburg@n0200.savio3>
2025-03-05 10:02:28 -0800

    Residual activation regularization experiments

--- scripts/trainer-residual-borzoi-regsweep_savio4-1gpu.sh
+++ scripts/trainer-residual-borzoi-regsweep_savio4-1gpu.sh
@@ -11,34 +11,19 @@
 #SBATCH --time=48:00:00
 #SBATCH --output=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.out
 #SBATCH --error=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.err
-#SBATCH --array=2,4,5 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
+#SBATCH --array=0-3 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
 #SBATCH --exclude=n0386.savio4
 # exclude node 386 temporarily because it has a hardware issue
 # Command(s) to run:
 # Define an array of config files
 CONFIG_FILES=(
-    "../configs/residual/borzoi_pretrain_head_only.gin"
-    "../configs/residual/methylseq-only_borzoi-dataset.gin"
-    "../configs/residual/borzoi_pretrain_methylseq-residual_multiply.gin"
-    "../configs/residual/borzoi_pretrain-full_methylseq-residual_multiply.gin"
-    "../configs/residual/borzoi_pretrain_seq-residual_multiply.gin"
-    "../configs/residual/borzoi_pretrain_smoothed-methyl-residual_multiply.gin")
+    "../configs/residual/regularization_sweep/borzoi_pretrain_0reg_methylseq-residual_multiply.gin"
+    "../configs/residual/regularization_sweep/borzoi_pretrain_0.00001reg_methylseq-residual_multiply.gin"
+    "../configs/residual/regularization_sweep/borzoi_pretrain_0.001reg_methylseq-residual_multiply.gin"
+    "../configs/residual/regularization_sweep/borzoi_pretrain_0.1reg_methylseq-residual_multiply.gin"
+)
 
-START_FROM_SLURM_ID=""
-
-for arg in "$@"; do
-    case $arg in
-        --start-from-slurm-id=*)
-            START_FROM_SLURM_ID="${arg#*=}"
-            shift
-            ;;
-        *)
-            shift
-            ;;
-    esac
-done
-
-START_CHECKPOINT="slurm${START_FROM_SLURM_ID}task${SLURM_ARRAY_TASK_ID}"
+START_CHECKPOINT="slurm23966228task2"
 
 echo $START_CHECKPOINT
 
