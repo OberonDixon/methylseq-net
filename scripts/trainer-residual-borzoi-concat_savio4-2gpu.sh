f10c02ac8388cfdf86a1dc15efd59f4100b06b10
Oberon Dixon-Luinenburg <dixonluinenburg@n0071.savio3>
2025-04-14 14:34:39 -0700

    Added co-train config for concat tests; do the benefits compound?

--- scripts/trainer-residual-borzoi-concat_savio4-2gpu.sh
+++ scripts/trainer-residual-borzoi-concat_savio4-2gpu.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --output=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.out
 #SBATCH --error=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.err
-#SBATCH --array=1-7 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
+#SBATCH --array=8-9 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
 #SBATCH --exclude=n0386.savio4
 # exclude node 386 temporarily because it has a hardware issue
 # Command(s) to run:
@@ -25,7 +25,8 @@ CONFIG_FILES=(
     "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basset_concat-emb-final-add.gin"
     "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basset_concat-emb-penult-add.gin"
     "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basset-add.gin"
-    
+    "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basset_concat-emb-final-cotrain38.gin"
+    "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basset-cotrain38.gin"
 )
 
 START_CHECKPOINT="slurm23966228task0"
