74ce913c72ae21fa62647b8846659554c0f6a27c
OberonDixon <oberondixon@gmail.com>
2025-10-03 20:42:49 -0700

    Final adjustments to fix basenji concat penult; now performs similarly generally a bit better than basset

--- scripts/trainer-residual-borzoi-concat_savio3-2gpu-cotrains.sh
+++ scripts/trainer-residual-borzoi-concat_savio3-2gpu-cotrains.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --output=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.out
 #SBATCH --error=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.err
-#SBATCH --array=1-1 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
+#SBATCH --array=3,5 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
 #SBATCH --exclude=n0134.savio3,n0135.savio3,n0136.savio3,n0137.savio3,n0138.savio3,n0143.savio3,n0144.savio3,n0145.savio3,n0158.savio3,n0159.savio3,n0160.savio3,n0161.savio3,n0174.savio3,n0175.savio3,n0176.savio3
 # Command(s) to run:
 # Define an array of config files
@@ -21,6 +21,7 @@ CONFIG_FILES=(
     "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basset-cotrain.gin"
     "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basset_concat-emb-final-cotrain.gin"
     "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basset_concat-emb-penult-cotrain.gin"
+    "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basenji_concat-emb-penult-cotrain.gin"
 )
 
 # START_CHECKPOINT="slurm28108438task1"
--- scripts/trainer-residual-borzoi-concat_savio3-2gpu.sh
+++ scripts/trainer-residual-borzoi-concat_savio3-2gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train_methylseqnet_concat_2gpu
 #SBATCH --account=fc_nilah
-#SBATCH --partition=savio4_gpu
+#SBATCH --partition=savio3_gpu
 #SBATCH --qos=savio_lowprio
 #SBATCH --nodes=1
 #SBATCH --ntasks=2
@@ -11,7 +11,8 @@
 #SBATCH --time=48:00:00
 #SBATCH --output=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.out
 #SBATCH --error=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.err
-#SBATCH --array=3,7 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
+#SBATCH --array=7 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
+#SBATCH --exclude=n0134.savio3,n0135.savio3,n0136.savio3,n0137.savio3,n0138.savio3,n0143.savio3,n0144.savio3,n0145.savio3,n0158.savio3,n0159.savio3,n0160.savio3,n0161.savio3,n0174.savio3,n0175.savio3,n0176.savio3
 # Command(s) to run:
 # Define an array of config files
 CONFIG_FILES=(
@@ -37,13 +38,13 @@ CONFIG_FILES=(
     # "../configs/residual/concat_pretrained_embeddings/borzoi_residual-methylseq-basenji_concat-emb-final-aug-6folds.gin"
 )
 
-START_CHECKPOINT="slurm28108438task1"
+# START_CHECKPOINT="slurm28108438task1"
 
-echo $START_CHECKPOINT
+# echo $START_CHECKPOINT
 
 # Get the config file for this array task
 CONFIG_FILE=${CONFIG_FILES[$SLURM_ARRAY_TASK_ID]}
 
 UNIQUE_IDENTIFIER="slurm${SLURM_ARRAY_JOB_ID}task${SLURM_ARRAY_TASK_ID}"
 source activate methylseqnet-prod
-NCCL_P2P_DISABLE=1 python ../methylseqnet/trainer.py --config $CONFIG_FILE --unique_identifier $UNIQUE_IDENTIFIER --batch_size 1 --logging-level DEBUG --start-from-checkpoint $START_CHECKPOINT
+NCCL_P2P_DISABLE=1 python ../methylseqnet/trainer.py --config $CONFIG_FILE --unique_identifier $UNIQUE_IDENTIFIER --batch_size 1 --logging-level DEBUG # --start-from-checkpoint $START_CHECKPOINT
