795282e28b24beaf39dc6837c9a9dfb8e428a9f6
OberonDixon <oberondixon@gmail.com>
2026-03-03 14:38:47 -0800

    Add more paths to support analysis pipeline

--- methylseqnet/paths.py
+++ methylseqnet/paths.py
@@ -18,6 +18,16 @@ with open(_CONFIG_PATH, "rb") as f:
 pacbio_5mC_tracks = Path(_config["pacbio_5mC_tracks"])
 fiberseq_tracks = Path(_config["fiberseq_tracks"])
 rna_tracks = Path(_config["rna_tracks"])
-preprocessed_datasets = Path(_config["preprocessed_datasets"])
+
+methylation_atlas = Path(_config["methylation_atlas"])
+atac_atlas = Path(_config["atac_atlas"])
+cage_atlas = Path(_config["cage_atlas"])
+
 genomes = Path(_config["genomes"])
+haplotypes = Path(_config["haplotypes"])
+
+preprocessed_datasets = Path(_config["preprocessed_datasets"])
 model_checkpoints = Path(_config["model_checkpoints"])
+
+analysis_pickles = Path(_config["analysis_pickles"])
+annotations = Path(_config["annotations"])
