69b15b79900f262e1bb44635ef58aa2b1702c371
OberonDixon <oberondixon@gmail.com>
2026-03-03 22:03:34 -0800

    Add new bassetlike models

--- scripts/train-all.sh
+++ scripts/train-all.sh
@@ -12,7 +12,7 @@
 #SBATCH --output=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.out
 #SBATCH --error=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.err
 #SBATCH --exclude=n0217.savio3,n0215.savio3,n0130.savio4,n0132.savio4,n0134.savio3,n0135.savio3,n0136.savio3,n0137.savio3,n0138.savio3,n0143.savio3,n0144.savio3,n0145.savio3,n0158.savio3,n0159.savio3,n0160.savio3,n0161.savio3,n0174.savio3,n0175.savio3,n0176.savio3
-#SBATCH --array=4-4 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
+#SBATCH --array=32-35 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
 # Command(s) to run:
 # Define an array of config files
 CONFIG_FILES=(
@@ -48,6 +48,10 @@ CONFIG_FILES=(
     "../configs/manuscript_training/bassetlike_methylseq-only_atlas+longread_smoothed.gin"
     "../configs/manuscript_training/borzoi-rep0_factorized_longread_imputed.gin"
     "../configs/manuscript_training/borzoi-rep0_factorized_longread_true.gin"
+    "../configs/manuscript_training/bassetlike_methylseq-only_atlas+longread_smoothed1k.gin"
+    "../configs/manuscript_training/bassetlike_methylseq-only_atlas+longread_smoothed8k.gin"
+    "../configs/manuscript_training/bassetlike_seq-only_atlas+longread.gin"
+    "../configs/manuscript_training/bassetlike_methyl-only_atlas+longread_smoothed.gin"
 )
 
 # Get the config file for this array task
