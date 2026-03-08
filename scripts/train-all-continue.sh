4c3b7aea086a94ddc6cc42a8b7ed53dd004ee857
OberonDixon <oberondixon@gmail.com>
2026-03-07 18:35:12 -0800

    Fix job id for task16 continue

--- scripts/train-all-continue.sh
+++ scripts/train-all-continue.sh
@@ -12,7 +12,7 @@
 #SBATCH --output=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.out
 #SBATCH --error=/clusterfs/nilah/oberon/lightning/sbatch_logs/train_methylseqnet_%A_%a.err
 #SBATCH --exclude=n0217.savio3,n0215.savio3,n0130.savio4,n0132.savio4,n0134.savio3,n0135.savio3,n0136.savio3,n0137.savio3,n0138.savio3,n0143.savio3,n0144.savio3,n0145.savio3,n0158.savio3,n0159.savio3,n0160.savio3,n0161.savio3,n0174.savio3,n0175.savio3,n0176.savio3
-#SBATCH --array=16,20-22,24,31 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
+#SBATCH --array=32,34,35 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
 # Command(s) to run:
 # Define an array of config files
 CONFIG_FILES=(
@@ -71,7 +71,7 @@ UNIQUE_IDENTIFIERS=(
     "slurm32260926task13"
     "slurm32263095task14"
     "slurm32263095task15"
-    "slurm32266095task16"
+    "slurm32263095task16"
     "slurm32263095task17"
     "slurm32263095task18"
     "slurm32263095task19"
