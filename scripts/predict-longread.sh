e1a49694838a5e59ff9e53c2de9f76061db81465
OberonDixon <oberondixon@gmail.com>
2026-03-12 11:37:27 -0700

    predict scripts for all 35 model experiments

--- scripts/predict-longread.sh
+++ scripts/predict-longread.sh
@@ -12,13 +12,47 @@
 #SBATCH --output=/clusterfs/nilah/oberon/lightning/sbatch_logs/predict_methylseqnet_%A_%a.out
 #SBATCH --error=/clusterfs/nilah/oberon/lightning/sbatch_logs/predict_methylseqnet_%A_%a.err
 #SBATCH --exclude=n0005.savio3,n0217.savio3,n0215.savio3,n0130.savio4,n0132.savio4,n0134.savio3,n0135.savio3,n0136.savio3,n0137.savio3,n0138.savio3,n0143.savio3,n0144.savio3,n0145.savio3,n0158.savio3,n0159.savio3,n0160.savio3,n0161.savio3,n0174.savio3,n0175.savio3,n0176.savio3
-#SBATCH --array=0-2 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
+#SBATCH --array=1,3,7-9,11-15,17,19-28,30,31,34,35 # Specify the range of array jobs (e.g., 0-2 for 3 configs)
 # Command(s) to run:
 # Define an array of config files
 KWARGS_ARRAY=(
-    "--model-identifier slurm31983102task0 --true-conditioning-state-weight 1.0"
-    "--model-identifier slurm31983102task0 --true-conditioning-state-weight 0.0"
-    "--model-identifier slurm31986468task1"
+    "--model-identifier slurm32260895task0"
+    "--model-identifier slurm32260895task1"
+    "--model-identifier slurm32260895task2"
+    "--model-identifier slurm32260895task3"
+    "--model-identifier slurm32273742task4"
+    "--model-identifier slurm32260895task5"
+    "--model-identifier slurm32260895task6"
+    "--model-identifier slurm32260895task7"
+    "--model-identifier slurm32260895task8"
+    "--model-identifier slurm32260895task9"
+    "--model-identifier slurm32260895task10"
+    "--model-identifier slurm32260895task11"
+    "--model-identifier slurm32260926task12"
+    "--model-identifier slurm32260926task13"
+    "--model-identifier slurm32263095task14"
+    "--model-identifier slurm32263095task15"
+    "--model-identifier slurm32263095task16"
+    "--model-identifier slurm32263095task17"
+    "--model-identifier slurm32263095task18"
+    "--model-identifier slurm32263095task19"
+    "--model-identifier slurm32263095task20"
+    "--model-identifier slurm32263095task21"
+    "--model-identifier slurm32263095task22"
+    "--model-identifier slurm32263095task23"
+    "--model-identifier slurm32263095task24"
+    "--model-identifier slurm32263095task25"
+    "--model-identifier slurm32263095task26"
+    "--model-identifier slurm32263821task27"
+    "--model-identifier slurm32263821task28"
+    "--model-identifier slurm32263821task29"
+    "--model-identifier slurm32265241task30"
+    "--model-identifier slurm32265241task31"
+    "--model-identifier slurm32292528task32"
+    "--model-identifier slurm32292528task33"
+    "--model-identifier slurm32292528task34"
+    "--model-identifier slurm32293065task35"
+    "--model-identifier slurm32260895task0 --true-conditioning-state-weight 1.0"
 )
 
 # Get the config file for this array task
